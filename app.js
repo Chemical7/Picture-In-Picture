43e47fa918142991ec7a232b42985e58fa2f9bca
Chemical7 <brianleputu@gmail.com>
2023-01-04 12:02:52 +0200

    add: video player
    - mediaStream API

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On Load
+selectMediaStream();
